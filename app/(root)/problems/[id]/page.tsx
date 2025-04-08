0acac6e52f50b607a95084bc1aab5cf14f834408
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-04-09 02:20:36 +0530

    user-authentication and middleware added

--- app/(root)/problems/[id]/page.tsx
+++ app/(root)/problems/[id]/page.tsx
@@ -1,3 +1,4 @@
+import EmptyState from '@/components/shared/empty-state';
 import { getProblemById } from '@/features/problems/actions'
 import ProblEditor from '@/features/problems/components/problem-editor';
 import React from 'react'
@@ -5,12 +6,11 @@ import React from 'react'
 const page = async({params}:{params:Promise<{id:string}>}) => {
     const paramsId =  (await (params)).id
     const problemData = await getProblemById(paramsId);
-console.log(problemData)
 
 
   return (
     // @ts-ignore
-    problemData ? <ProblEditor data={problemData} /> : <div>Problem not found</div>
+    problemData ? <ProblEditor data={problemData} /> : <EmptyState imageUrl='/empty-state.svg' title='Problem not found' description='The problem you are looking for does not exist or has been removed.' onClick={() => window.location.reload()}/>
   )
 }
 
